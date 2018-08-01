2375abbf2c1ad76c5af56e22e19cd29b51716b96
Paul Carrillo <paulcarrillo05@gmail.com>
2018-07-31 22:23:18 -0600

    Testing is complete all tests are green

---
+++ README.md
@@ -0,0 +1,36 @@
+# Feed Reader Testing
+
+## Installation
+
+How to successfully run the application
+
+1. Fork the  repo [GitHub Pages](https://help.github.com/articles/fork-a-repo/).
+2. `cd` into desired folder where you would like the file to live.
+3. Using `git clone` copy the file on to your machine.
+4. Once file is installed testing can be viewed by simply opening the html to a browser.
+
+
+## Why this Project?
+
+Testing is an important part of the development process many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
+
+Tests are used to make sure future feature development doesn't break existing features!
+
+This projects uses Jasmine for testing functionality.
+
+
+## Resources
+
+* [Jasmine](https://jasmine.github.io/pages/getting_started.html).
+* [jQuery](https://learn.jquery.com/using-jquery-core/document-ready/).
+* [Handlebars](https://handlebarsjs.com/).
+* [Mozilla](https://developer.mozilla.org/en-US/).
+
+
+## Author
+
+* [Paul Carrillo](https://www.linkedin.com/in/paul-carrillo/)
+
+## License
+
+MIT
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,105 @@
+/* This is the spec file that Jasmine will read it contains
+ * all of the tests that will be run against the application.
+ */
+
+/* All of the tests are place within the $() function,
+ * since some of these tests require DOM elements.
+ */
+$(function() {
+    // Test suite for the RSS feed variable //
+    describe('RSS Feeds', function() {
+
+        // Test if allFeeds variable exists and make sure that it is not empty.//
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+        /* Test loops through each feed in the allFeeds object and ensures
+         * it has a URL defined and that the URL is not empty.
+         */
+         it('urls defined', function() {
+           for(let feed of allFeeds) {
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           }
+         });
+
+
+        /* Test loops through each feed in the allFeeds object and ensures
+         * that it has a name defined and that the name is not empty.
+         */
+         it('names defined', function() {
+           for(let feed of allFeeds) {
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).not.toBe(0);
+           }
+         })
+    });
+
+
+    // Test suite for the menu element inside the html body. //
+    describe('The Menu', function() {
+
+        // Test ensures that the menu element is hidden by default. //
+        it('is hidden', function() {
+           const body = document.querySelector('body');
+           expect(body.classList.contains('menu-hidden')).toBe(true);
+         });
+
+         /* Test ensures the menu changes visibility when the menu icon is
+          * clicked. Menu displays when clicked and hides when clicked again.
+          */
+          it('toggle completes', function() {
+            const body = document.querySelector('body');
+            const menu = document.querySelector('.menu-icon-link');
+
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(false);
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+          });
+     });
+
+    // Test suite for the loadFeed function //
+    describe('Initial Entries', function() {
+
+        /* Test that ensures the loadFeed function
+         * is called and completes its work,
+         */
+         beforeEach(function(done) {
+           loadFeed(0, done);
+         });
+
+         it('feeds loaded', function() {
+           const feed = document.querySelector('.feed');
+           expect(feed.children.length > 0).toBe(true);
+         });
+    });
+
+    // Test suite New Feed Selection //
+    describe('New Feed Selection', function() {
+
+        const container = document.querySelector('.feed');
+        const firstFeed = [];
+
+         /* Test loads muiltiple feeds and compares content when a new
+          * feed is loaded by the loadFeed functionand checks that the
+          * content actually changes.
+          */
+
+        beforeEach(function(done) {
+            loadFeed(0);
+            Array.from(container.children).forEach(content => {
+                firstFeed.push(content.innerText);
+            });
+            loadFeed(1, done);
+        });
+        it('content changes', function() {
+            Array.from(container.children).forEach( (content, index) => {
+                expect(content.innerText !== firstFeed[index]).toBe(true);
+            });
+        });
+    });
+}());
